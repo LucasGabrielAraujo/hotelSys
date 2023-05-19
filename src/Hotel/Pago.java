a54b777c21b2d356ce12fd4154a976c15671d6eb
LucasGabrielAraujo <122766033+LucasGabrielAraujo@users.noreply.github.com>
2023-05-19 17:30:25 -0300

    Update Pago.java

--- src/Hotel/Pago.java
+++ src/Hotel/Pago.java
@@ -1,6 +1,17 @@
 package Hotel;
 
 public class Pago {
-    int numeroDeTarjeta;
+	int numeroDeTarjeta;
 	int tipoPago;
+	
+	public Pago(int numeroDeTarjeta, int tipoPago){
+		this.numeroDeTarjeta=numeroDeTarjeta;
+		this.tipoPago=tipoPago;
+	}
+	public int obtenerNumero(){
+		return this.numeroDeTarjeta;
+	}
+	public int obtenerTipoPago(){
+		return this.tipoPago;
+	}
 }
