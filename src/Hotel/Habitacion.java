f0f48e7426e5feb85f72d6134176c95bb2c24c6f
Marcos-Aguero <yonidj52@hotmail.com>
2023-05-19 17:37:52 -0300

    Add: se agregan clase

---
+++ src/Hotel/Habitacion.java
@@ -0,0 +1,47 @@
+package Hotel;
+
+public class Habitacion {
+    private boolean disponible = true;
+    private int[] habitacionesDisponibles;
+    private String tipoHabitacion;
+
+    public Habitacion(int[] habitacionesDisponibles, String tipoHabitacion) {
+        this.habitacionesDisponibles = habitacionesDisponibles;
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int[] getHabitacionesDisponibles() {
+        return habitacionesDisponibles;
+    }
+
+    public void setHabitacionesDisponibles(int[] habitacionesDisponibles) {
+        this.habitacionesDisponibles = habitacionesDisponibles;
+    }
+
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Habitacion{" + "disponible=" + disponible + ", habitacionesDisponibles=" + habitacionesDisponibles + ", tipoHabitacion=" + tipoHabitacion + '}';
+    }
+    
+    public void obtenerHabitacionesDisponibles(){
+        
+    }
+    
+
+}
---
+++ src/Hotel/Paquete.java
@@ -0,0 +1,45 @@
+package Hotel;
+
+import java.util.Scanner;
+
+public class Paquete {
+    private String nombrePaquete;
+    private double precio;
+
+    public Paquete(String nombrePaquete, double precio) {
+        this.nombrePaquete = nombrePaquete;
+        this.precio = precio;
+    }
+
+    public String getNombrePaquete() {
+        return nombrePaquete;
+    }
+
+    public void setNombrePaquete(String nombrePaquete) {
+        this.nombrePaquete = nombrePaquete;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "nombrePaquete=" + nombrePaquete + ", precio=" + precio + '}';
+    }
+    
+    public void obtenerPaquete(){
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese el Paquete elegido: ");
+        nombrePaquete = entrada.nextLine();
+    }
+    
+    public void obtenerPrecio(){
+        
+    }
+}
