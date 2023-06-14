f77677580427a4f9cab065d9afae15a9c18c47f8
Lucas <lucasgabrielaraujo13.08@gmail.com>
2023-06-14 16:29:36 -0300

    Reservas parcialmente terminadas

--- src/Hotel/Main.java
+++ src/Hotel/Main.java
@@ -1,24 +1,33 @@
 package Hotel;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Date inicio;
-        Date fin;
+        // Variables para almacenar la selección del usuario
+        List<Hotel> listaHoteles = new ArrayList<>();
+        Hotel hotelSeleccionado = null;
+        ReservarHotel reserva = null;
+        Pago pago = null;
         Scanner scanner = new Scanner(System.in);
+
         Hotel hotel = new Hotel("Hotel Azul", 10, 4, 100, "Peru",
                 "Productos Ejemplo", "Servicios Ejemplo", 50);
         Hotel hotel1 = new Hotel("Hotel Verde", 0, 5, 300, "Argentina",
                 "Productos Ejemplo", "Servicios Ejemplo", 50);
         Hotel hotel2 = new Hotel("Hotel Blanco", 10, 4, 522, "Brasil",
                 "Productos Ejemplo", "Servicios Ejemplo", 50);
-        Hotel[] hoteles = new Hotel[] { hotel1, hotel2, hotel };
+
         Cliente cliente = new Cliente("Lucas", "Araujo", "42830058", "Casual 123", "Lucas Araujo", "lucas@mail.com",
                 "123", 532512, "Lucas123", "caca.mp");
-
+        listaHoteles.add(hotel);
+        listaHoteles.add(hotel1);
+        listaHoteles.add(hotel2);
         int opcion;
         do {
             menuHoteles();
@@ -27,7 +36,7 @@ public class Main {
             System.out.println("---------------------------------------");
             switch (opcion) {
                 case 1:
-                    for (Hotel hotelPos : hoteles) {
+                    for (Hotel hotelPos : listaHoteles) {
                         System.out.println("Nombre: " + hotelPos.getNombre());
                         System.out.println("Pais: " + hotelPos.getPais());
                         System.out.println("Capacidad: " + hotelPos.getCapacidad());
@@ -50,7 +59,7 @@ public class Main {
                                 System.out.print("Ingrese nombre: ");
                                 hotelAux = scanner.nextLine();
                                 encontrado = false;
-                                for (Hotel hotelN : hoteles) {
+                                for (Hotel hotelN : listaHoteles) {
                                     if (hotelN.getNombre().equals(hotelAux)) {
                                         System.out.println("---------------------------------------");
                                         System.out.println("Nombre: " + hotelN.getNombre());
@@ -68,88 +77,13 @@ public class Main {
                                     System.out.println("---------------------------------------");
                                     System.out.println("¡Hoteles no encontrados!");
                                     System.out.println("---------------------------------------");
-                                } else {
-                                    menuVerHoteles();
-                                    opcion = scanner.nextInt();
-                                    int auxNum = 0;
-                                    encontrado = false;
-                                    switch (opcion) {
-                                        case 1:
-                                            System.out.println("Ingrese el nombre del Hotel: ");
-                                            hotelAux = scanner.nextLine();
-                                            for (int i = 0; i < hoteles.length; i++) {
-                                                if (hoteles[i].getNombre().equals(hotelAux)) {
-                                                    auxNum = i;
-                                                    encontrado = true;
-                                                    break;
-                                                }
-                                            }
-                                            if (!encontrado) {
-                                                System.out.println("---------------------------------------");
-                                                System.out.println("¡Hoteles no encontrados!");
-                                                System.out.println("---------------------------------------");
-                                            } else {
-                                                menuReserva();
-                                                opcion = scanner.nextInt();
-                                                switch (opcion) {
-                                                    case 1:
-                                                        if (hoteles[auxNum].verificarDisponibilidad()) {
-                                                            ReservarHotel reserva = new ReservarHotel(cliente, hotel,
-                                                                    null,
-                                                                    null);
-                                                            if (reserva.hacerReserva()) {
-                                                                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                                                                System.out.print(
-                                                                        "Ingrese la fecha de inicio de la reserva(dd/MM/yyyy): ");
-                                                                String fechaInicioStr = scanner.nextLine();
-                                                                inicio=null;
-                                                                System.out.print(
-                                                                        "Ingrese la fecha de fin de la reserva(dd/MM/yyyy): ");
-                                                                String fechaFinStr = scanner.nextLine();
-                                                                fin=null;
-                                                                try{
-                                                                    inicio = dateFormat.parse(fechaInicioStr);
-                                                                }catch(Exception e){
-                                                                    System.out.println("Error al ingresar la fecha de inicio.");
-                                                                    break;
-                                                                }
-                                                                try{
-                                                                    fin = dateFormat.parse(fechaFinStr);
-                                                                }catch(Exception e){
-                                                                    System.out.println("Error al ingresar la fecha de fin.");
-                                                                    break;
-                                                                }
-                                                                reserva.setFechaInicio(inicio);
-                                                                reserva.setFechaFin(fin);
-                                                                System.out.println("Reserva realizada con éxito.");
-                                                            } else {
-                                                                System.out.println(
-                                                                        "No hay habitaciones disponibles para reservar.");
-                                                            }
-                                                        } else {
-                                                            System.out.println(
-                                                                    "No hay habitaciones disponibles para reservar.");
-                                                        }
-
-                                                        break;
-
-                                                    default:
-                                                        break;
-                                                }
-                                            }
-                                            break;
-                                        case 2:
-                                            break;
-                                        default:
-                                            break;
-                                    }
                                 }
                                 break;
                             case 2:
                                 System.out.print("Ingrese precio: ");
                                 hotelAux = scanner.nextLine();
                                 encontrado = false;
-                                for (Hotel hotelN : hoteles) {
+                                for (Hotel hotelN : listaHoteles) {
                                     if (hotelN.getPais().equals(hotelAux)) {
                                         System.out.println("---------------------------------------");
                                         System.out.println("Nombre: " + hotelN.getNombre());
@@ -173,7 +107,7 @@ public class Main {
                                 System.out.print("Ingrese pais: ");
                                 int hotelPrecio = scanner.nextInt();
                                 encontrado = false;
-                                for (Hotel hotelN : hoteles) {
+                                for (Hotel hotelN : listaHoteles) {
                                     if (hotelN.getPrecio() <= hotelPrecio) {
                                         System.out.println("---------------------------------------");
                                         System.out.println("Nombre: " + hotelN.getNombre());
@@ -195,7 +129,7 @@ public class Main {
                                 break;
                             case 4:
                                 encontrado = false;
-                                for (Hotel hotelN : hoteles) {
+                                for (Hotel hotelN : listaHoteles) {
                                     if (hotelN.verificarDisponibilidad()) {
                                         System.out.println("---------------------------------------");
                                         System.out.println("Nombre: " + hotelN.getNombre());
@@ -222,9 +156,116 @@ public class Main {
                                 break;
                         }
                     } while (opcion != 5);
-
                     break;
                 case 3:
+                    do {
+
+                        menuReserva();
+                        opcion = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.println("---------------------------------------");
+                        switch (opcion) {
+                            case 1:
+                                // Solicitar al usuario que ingrese el nombre del hotel deseado
+                                System.out.print("Ingrese el nombre del hotel deseado: ");
+                                String nombreHotel = scanner.nextLine();
+                                scanner.nextLine();
+                                // Buscar el hotel en la lista de hoteles disponibles
+                                for (Hotel hoteln : listaHoteles) {
+                                    if (hoteln.getNombre().equalsIgnoreCase(nombreHotel)) {
+                                        hotelSeleccionado = hoteln;
+                                        break;
+                                    }
+                                }
+
+                                // Verificar si se encontró el hotel
+                                if (hotelSeleccionado != null) {
+                                    System.out.println("Hotel seleccionado: " + hotelSeleccionado.getNombre());
+                                    System.out.println("---------------------------------------");
+                                } else {
+                                    System.out.println(
+                                            "El hotel ingresado no está disponible. Por favor, seleccione otro hotel.");
+                                    System.out.println("---------------------------------------");
+                                }
+                                break;
+                            case 2:
+                                // Verificar si se ha seleccionado un hotel
+                                if (hotelSeleccionado == null) {
+                                    System.out.println("Primero debe seleccionar un hotel.");
+                                    System.out.println("---------------------------------------");
+                                    break;
+                                }
+
+                                // Solicitar al usuario las fechas de inicio y fin de la reserva
+                                System.out.print("Ingrese la fecha de inicio de la reserva (dd/MM/yyyy): ");
+                                String fechaInicioStr = scanner.nextLine();
+                                System.out.print("Ingrese la fecha de fin de la reserva (dd/MM/yyyy): ");
+                                String fechaFinStr = scanner.nextLine();
+
+                                // Convertir las cadenas de fecha a objetos Date
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                                Date fechaInicio;
+                                Date fechaFin;
+                                try {
+                                    fechaInicio = dateFormat.parse(fechaInicioStr);
+                                    fechaFin = dateFormat.parse(fechaFinStr);
+                                } catch (ParseException e) {
+                                    System.out.println(
+                                            "Formato de fecha inválido. Por favor, ingrese las fechas en el formato correcto.");
+                                    System.out.println("---------------------------------------");
+                                    break;
+                                }
+
+                                // Crear una instancia de ReservarHotel
+                                reserva = new ReservarHotel(cliente, hotelSeleccionado, fechaInicio,
+                                        fechaFin);
+
+                                // Realizar la reserva
+                                boolean reservaExitosa = reserva.hacerReserva();
+
+                                if (reservaExitosa) {
+                                    System.out.println("Reserva realizada exitosamente.");
+                                    System.out.println("---------------------------------------");
+                                } else {
+                                    System.out.println(
+                                            "No se pudo realizar la reserva. El hotel no tiene habitaciones disponibles.");
+                                    System.out.println("---------------------------------------");
+                                }
+                                break;
+                            case 3:
+                                // Verificar si se ha realizado una reserva
+                                if (reserva == null) {
+                                    System.out.println("Primero debe realizar una reserva.");
+                                    break;
+                                }
+
+                                // Solicitar al usuario el monto y método de pago
+                                System.out.print("Ingrese el monto a pagar: ");
+                                double monto = scanner.nextDouble();
+                                scanner.nextLine(); // Consumir la nueva línea pendiente
+                                System.out.print("Ingrese el método de pago: ");
+                                String metodoPago = scanner.nextLine();
+
+                                // Crear una instancia de Pago
+                                pago = new Pago(reserva, monto, metodoPago);
+
+                                // Realizar el pago
+                                boolean pagoExitoso = pago.realizarPago(hotel);
+
+                                if (pagoExitoso) {
+                                    System.out.println("Pago realizado exitosamente.");
+                                    // Generar el ticket con la reserva y el pago
+                                    generarTicket(reserva, pago, hotelSeleccionado);
+                                } else {
+                                    System.out.println(
+                                            "No se pudo realizar el pago. El monto ingresado es insuficiente.");
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    } while (opcion != 4);
+
                     /*
                      * System.out.print("Ingrese el monto del pago: ");
                      * double montoPago = scanner.nextDouble();
@@ -256,12 +297,13 @@ public class Main {
             }
 
             System.out.println();
-        } while (opcion != 3);
+        } while (opcion != 4);
 
         scanner.close();
+
     }
 
-    public static void imprimirTicket(ReservarHotel reserva, Pago pago, Hotel hotel) {
+    public static void generarTicket(ReservarHotel reserva, Pago pago, Hotel hotel) {
         System.out.println("---------- Ticket de Reserva ----------");
         System.out.println("Hotel: " + reserva.getHotel().getNombre());
         System.out.println("Cliente: " + reserva.getCliente().getNombre() + " " + reserva.getCliente().getApellido());
@@ -274,7 +316,7 @@ public class Main {
     }
 
     public static void menuReserva() {
-        System.out.println("1. Verificar disponibilidad");
+        System.out.println("1. Seleccionar hotel");
         System.out.println("2. Realizar reserva");
         System.out.println("3. Realizar pago");
         System.out.println("4. Salir");
@@ -287,7 +329,8 @@ public class Main {
         System.out.println("---------------------------------------");
         System.out.println("1. Ver hoteles");
         System.out.println("2. Filtrar hoteles");
-        System.out.println("3. Salir");
+        System.out.println("3. Reservar hotel");
+        System.out.println("4. Salir");
         System.out.print("Ingrese una opción: ");
     }
 
